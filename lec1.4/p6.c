ebdd1de12f86189ae2c4ec11a04bda305f04d5ec
khushi-chauhan32 <khushirchauhan32@gmail.com>
2024-03-27 18:30:46 +0530

    lecture1.4

---
+++ lec1.4/p6.c
@@ -0,0 +1,25 @@
+//2 variabal and ans print//
+#include<stdio.h>
+main(){
+	int num1=200;
+	int num2=500;
+	printf("%d+%d=%d\n",num1,num2,num1+num2);
+	
+	
+	int num3=100;
+	int num4=66;
+	printf("%d-%d=%d\n",num3,num4,num3-num4);
+	
+	int num5=54;
+	int num6=79;
+	printf("%d*%d=%d\n",num5,num6,num5*num6);
+	
+	int num7=25;
+	int num8=5;
+	printf("%d/%d=%d\n",num7,num8,num7/num8);
+	
+	
+	int num9=8;
+	int num10=8;
+	printf("8%8=%d",num9,num10,8 % 8);
+}
